e69f37dd5bbc4709a4e41d32f59b4996ffd7251a
Devesh <67568465+deveshksrivastava@users.noreply.github.com>
2024-02-16 05:39:07 +0000

    added feature for animcation on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       aspectRatio: {
         '4/3': '4 / 3',
       },
-      backgroundImage:{
-        'play-back':"url('/play.jpg')"
+      backgroundImage: {
+        'play-back': "url('/play.jpg')",
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        zoomOut: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1.2)' },
+        },
       },
       animation: {
-
-      }
+        slideIn: 'slideIn 2s ease-in-out',
+        zoomOut: 'zoomOut 2s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
-}
-
+};
